0d1bd3f6c8b423722ce64dd04750285872e649ef
Yetichain <118218938+yetichain@users.noreply.github.com>
2023-01-06 19:51:56 -0600

    Update yetichain.sol

--- yetichain.sol
+++ yetichain.sol
@@ -35,6 +35,6 @@
 // SPDX-License-Identifier: MIT
 
 // Project: YETI Chain 👹
-// Description: The future of the web3 built on #YETI Chain
+// Description: The future of blockchain, allow developers to build their Web3 dApps fast & secure
 // Website: https://www.yetichain.com
 // Version: 1.0
